96a9148bde4f69dc59339d5b87b3bff960f1e4a0
timberlake2025 <timberlake2025@gmail.com>
2023-06-27 17:52:55 -0700

    Added rating prog, checked for provisional rating

--- bot_leaderboard.py
+++ bot_leaderboard.py
@@ -42,19 +42,21 @@ def get_bot_ratings_online(type):
     for i in online_bots:
         d = orjson.loads(i)
         try:
-            result = [d['username'], d['perfs'][type]['rating']]
+            result = [d['username'], d['perfs'][type]['rating'], d['perfs'][type]['prog']]
             print(count, result)
             if result[0] in banned_bots:
                 banned += 1
+            if d['perfs'][type]['prov'] == True:
+                print('Provisional rating')
             else:
                 user_arr.append(result)
         except:
-            print("No {type} rating available")
+            print("No " + type + " rating available")
         count += 1
     resulting_arr = sorted(user_arr, key=lambda x: x[1], reverse=True)
     with open(get_file_name(type), 'w') as f:
                 for j in resulting_arr:
-                    print(str(count2) + ". " + "@" + j[0] + ": " + str(j[1]), file=f)
+                    print(str(count2) + ". " + "@" + j[0] + "-> Rating: " + str(j[1]) + ", Prog: " + str(j[2]) , file=f)
                     count2 += 1
     return "\n" + "Banned bots: " + str(banned)
 
