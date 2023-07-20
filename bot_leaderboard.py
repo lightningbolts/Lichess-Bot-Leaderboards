1c4af542de2d55e3b3d62996618c3b79b6721120
Đinh Hoàng Việt <134517889+M-DinhHoangViet@users.noreply.github.com>
2023-07-20 08:06:54 +0700

    Add some banned bots
    Add 3 bots farming

--- bot_leaderboard.py
+++ bot_leaderboard.py
@@ -41,7 +41,10 @@ def get_bot_ratings_online(type):
         'RexherBot',
         'SamuraiX_v1',
         'YellowFlash_v2',
-        'Anand_Bot'
+        'Anand_Bot',
+        'VariantsONTop',
+        'WhatsANikitosikHUH',
+        'OkayWhyYouReadinThis'
     ]
     online_bots = urllib.request.urlopen('https://lichess.org/api/bot/online')
     user_arr = []
